56bdb07be9306704c6ce8279e8bc40d7c4dace80
geonmo <ry840901@gmail.com>
2014-01-16 15:39:23 +0100

    Modified vallot_merge.py to support absolute path.

--- TopJpsi/result/vallot_merge.py
+++ TopJpsi/result/vallot_merge.py
@@ -8,10 +8,16 @@ if len(sys.argv) != 2 :
 	sys.stderr.write( "Example : ./vallot_merge.py ../../CMGAnalyzer/prod/Out/\n")
 	sys.exit(-1)
 dir = sys.argv[1]
+
 list_dir=[]
 for list in  os.listdir(dir):
 	if os.path.isdir(dir+list) :
-		list_dir.append(os.path.normpath(os.getcwd()+'/'+dir+list))	
+		if ( dir[0]=='/' ) :
+			list_dir.append(dir+list)
+		elif ( dir[0]=='.' ) :
+			list_dir.append(os.path.normpath(os.getcwd()+'/'+dir+list))	
+		else :
+			pass
 for sample_fullpath in list_dir :
 	sample_name = sample_fullpath.split('/')[-1]
 	vallot_dir = sample_fullpath+'/Res'
